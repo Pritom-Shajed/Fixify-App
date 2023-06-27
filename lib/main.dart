a6e1364779167416d33e48a3c015ed2e3019a1be
Pritom Shajed <pritomshajed@gmail.com>
2023-06-27 15:41:40 +0600

    auth page in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:fixify_app/pages/splashscreen/splashscreen_page.dart';
 import 'package:fixify_app/routes/route_helper.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
-import 'pages/auth/authentication_page.dart';
 import 'helper/dependencies.dart' as dep;
 
 Future main() async {
@@ -19,7 +16,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      builder: FToastBuilder(),
       debugShowCheckedModeBanner: false,
       title: 'Fixify',
       theme: ThemeData(
